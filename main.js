f3a81d4ff9c1d0c0e9ba75da29fb9eaddad3b861
Steffen-Svenningsen <steffen@zynaps.dk>
2022-12-09 15:50:39 +0100

    Create dropdown menu for links in the header

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+const activityMenu = document.getElementById('activityDropdown');
+const eventMenu = document.getElementById('eventDropdown');
+const activityLink = document.getElementById('activityLink');
+const eventLink = document.getElementById('eventLink');
+
+activityLink.addEventListener('mouseover', function() {
+  activityMenu.style.display = "flex";
+});
+
+activityMenu.addEventListener('mouseover', function() {
+  activityMenu.style.display = "flex";
+});
+
+activityMenu.addEventListener('mouseleave', function() {
+  activityMenu.style.display = "none";
+});
+
+activityLink.addEventListener('mouseleave', function() {
+  activityMenu.style.display = "none";
+});
+
+eventLink.addEventListener('mouseover', function() {
+  eventMenu.style.display = "flex";
+});
+
+eventMenu.addEventListener('mouseover', function() {
+  eventMenu.style.display = "flex";
+});
+
+eventMenu.addEventListener('mouseleave', function() {
+  eventMenu.style.display = "none";
+});
+
+eventLink.addEventListener('mouseleave', function() {
+  eventMenu.style.display = "none";
+});
+
+// Configuration for swiper.js plugin to create the 'customer reviews slider'.
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
