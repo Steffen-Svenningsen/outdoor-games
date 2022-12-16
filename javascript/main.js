5972eb79178e1ed0b9496c08f1d83b1748368d40
Steffen-Svenningsen <steffen@zynaps.dk>
2022-12-16 13:57:37 +0100

    Put swiper in it's own js file

--- javascript/main.js
+++ javascript/main.js
@@ -93,31 +93,6 @@ function scrollProgress() {
     document.getElementById('scrollBar').style.width = scrolled + "%";
 };
 
-// Configuration for swiper.js plugin to create the 'customer reviews slider'.
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    spaceBetween: 100,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // Enable autoplay
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-});
-
 // Get the video element from the DOM
 var promoVideo = document.getElementById("video")
 
